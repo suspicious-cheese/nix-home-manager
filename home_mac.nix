5d241a4735f912757bbabc03d27c2f1e32a72e16
Klaas van Loon <klaasvanloon@macbookpro-1.home>
2025-12-21 10:15:28 +0100

    attempting to do aliases

--- home_mac.nix
+++ home_mac.nix
@@ -25,13 +25,18 @@
   };
 
 
-
-xdg.configFile."micro/colorschemes/catppuccin-mocha.micro" = {
-  source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/micro/main/themes/catppuccin-mocha.micro";
-    sha256 = "sha256-pWO6m6w+5AsUXqJxTwHPG5puZ8PE+7JE7KcbXk8caDs=";
+  xdg.configFile."micro/colorschemes/catppuccin-mocha.micro" = {
+    source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/micro/main/themes/catppuccin-mocha.micro";
+      sha256 = "sha256-pWO6m6w+5AsUXqJxTwHPG5puZ8PE+7JE7KcbXk8caDs=";
+    };
   };
-};
+
+
+  home.shellAliases = {
+  	mi = "micro";
+  	hms = "home-manager switch --flake ~/.nix#chleese_mac";
+  };
   
   programs.home-manager.enable = true;
 }
