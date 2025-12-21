e39b05620d8a9140f932f7c05b658207b1bf3184
chleese <chleese@pop-os.home>
2025-12-21 17:49:22 +0100

    added bash aliases

--- home.nix
+++ home.nix
@@ -7,6 +7,7 @@
     hello
     micro
     bat
+    kdePackages.kmines
   ];
 
   programs.bat = {
@@ -26,12 +27,29 @@
 
 
 
-xdg.configFile."micro/colorschemes/catppuccin-mocha.micro" = {
-  source = pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/catppuccin/micro/main/themes/catppuccin-mocha.micro";
-    sha256 = "sha256-pWO6m6w+5AsUXqJxTwHPG5puZ8PE+7JE7KcbXk8caDs=";
+  xdg.configFile."micro/colorschemes/catppuccin-mocha.micro" = {
+    source = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/catppuccin/micro/main/themes/catppuccin-mocha.micro";
+      sha256 = "sha256-pWO6m6w+5AsUXqJxTwHPG5puZ8PE+7JE7KcbXk8caDs=";
+    };
   };
-};
+
+  programs.bash = {
+  	enable = true;
+  	bashrcExtra = ''
+  	    if [ -f ~/.bashrc.local ]; then
+  	      source ~/.bashrc.local
+  	    fi
+  	  '';
+
+  	shellAliases = {
+  		mi = "micro";
+		hms = "home-manager switch --flake ~/.nix#chleese";
+	    whs = "wormhole send";
+	    whr = "wormhole receive";
+  	};
+  };
+
   
   programs.home-manager.enable = true;
 }
